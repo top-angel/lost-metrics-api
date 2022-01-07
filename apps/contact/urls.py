f807814ce9c9c33e0c491d4fdedc74cdd4fddbcb
Abhinav <theabhinavdev@gmail.com>
2022-01-07 17:46:16 +0545

    LM-12, compatible with python 3.6

--- apps/contact/urls.py
+++ apps/contact/urls.py
@@ -4,8 +4,10 @@ from rest_framework import routers
 from .views import *
 
 router = routers.SimpleRouter()
+router.register('contact', ContactViewSet, basename='contact')
+router.register('interest', InterestViewSet, basename='interest')
 urlpatterns = [
     path('', include(router.urls)),
-    path('create_contact/', CreateContact.as_view(), name='create_contact'),
-    path('create_interest/', CreateInterest.as_view(), name='create_interest'),
+    # path('create_contact/', CreateContact.as_view(), name='create_contact'),
+    # path('create_interest/', CreateInterest.as_view(), name='create_interest'),
 ]
--- apps/contact/views.py
+++ apps/contact/views.py
@@ -2,6 +2,7 @@
 from django.utils.decorators import method_decorator
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.generics import CreateAPIView
+from rest_framework.viewsets import ModelViewSet
 
 from apps.contact.models import Contact, Interest
 from apps.contact.serializers import ContactSerializer, InterestSerializer
@@ -18,6 +19,41 @@ class CreateContact(CreateAPIView):
     queryset = Contact.objects.all()
 
 
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    tags=['Contact'],
+    operation_summary="List all the contacts",
+    operation_description="""List all API users. 
+    """
+))
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(
+    tags=['Contact'],
+    operation_summary="Get contact object",
+    operation_description="""Show full information for contact."""
+))
+@method_decorator(name='update', decorator=swagger_auto_schema(
+    tags=['Contact'],
+    operation_summary="Update a contact list",
+    operation_description="""Update contact object."""
+))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(
+    tags=['Contact'],
+    operation_summary="Partial Update a contact list",
+    operation_description="""Partial Update contact object."""
+))
+@method_decorator(name='create', decorator=swagger_auto_schema(
+    tags=['Contact'],
+    operation_summary="Create Contact",
+    operation_description="""Create a new contact"""
+))
+@method_decorator(name='destroy', decorator=swagger_auto_schema(
+    tags=['Contact'],
+    operation_summary="Delete Contact"
+))
+class ContactViewSet(ModelViewSet):
+    serializer_class = ContactSerializer
+    queryset = Contact.objects.all()
+
+
 @method_decorator(name='post',
                   decorator=swagger_auto_schema(
                       tags=['Contact'],
@@ -25,5 +61,40 @@ class CreateContact(CreateAPIView):
                       operation_description="Use this API to create Interest for a contact."
                   ))
 class CreateInterest(CreateAPIView):
+    serializer_class = InterestSerializer
+    queryset = Interest.objects.all()
+
+
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    tags=['Contact'],
+    operation_summary="List all the interests",
+    operation_description="""List all API users. 
+    """
+))
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(
+    tags=['Contact'],
+    operation_summary="Get interest object",
+    operation_description="""Show full information for interest."""
+))
+@method_decorator(name='update', decorator=swagger_auto_schema(
+    tags=['Contact'],
+    operation_summary="Update a interest list",
+    operation_description="""Update interest object."""
+))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(
+    tags=['Contact'],
+    operation_summary="Partial Update a interest list",
+    operation_description="""Partial Update interest object."""
+))
+@method_decorator(name='create', decorator=swagger_auto_schema(
+    tags=['Contact'],
+    operation_summary="Create Interest object",
+    operation_description="""Create a new interest"""
+))
+@method_decorator(name='destroy', decorator=swagger_auto_schema(
+    tags=['Contact'],
+    operation_summary="Delete Contact"
+))
+class InterestViewSet(ModelViewSet):
     serializer_class = InterestSerializer
     queryset = Interest.objects.all()
