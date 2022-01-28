933e19896793aa89c31664454cb2d255b5226d26
Abhinav <theabhinavdev@gmail.com>
2022-01-28 19:37:05 +0545

    LM-26, 27

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -2,8 +2,8 @@
 from django.utils.decorators import method_decorator
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.generics import CreateAPIView
-from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework import mixins
 from apps.contact.models import Contact, Interest
 from apps.contact.serializers import ContactSerializer, InterestSerializer
 
@@ -83,7 +83,7 @@ class CreateInterest(CreateAPIView):
 ))
 @method_decorator(name='partial_update', decorator=swagger_auto_schema(
     tags=['Contact'],
-    operation_summary="Partial Update a interest list",
+    operation_summary="Partial Update a Interest object",
     operation_description="""Partial Update interest object."""
 ))
 @method_decorator(name='create', decorator=swagger_auto_schema(
@@ -93,7 +93,7 @@ class CreateInterest(CreateAPIView):
 ))
 @method_decorator(name='destroy', decorator=swagger_auto_schema(
     tags=['Contact'],
-    operation_summary="Delete Contact"
+    operation_summary="Delete Interest"
 ))
 class InterestViewSet(ModelViewSet):
     serializer_class = InterestSerializer
@@ -106,18 +106,18 @@ class InterestViewSet(ModelViewSet):
 
 @method_decorator(name='list', decorator=swagger_auto_schema(
     tags=['Contact'],
-    operation_summary="List all the interests",
+    operation_summary="List all the contact interest",
     operation_description="""List all API users. 
     """
 ))
 @method_decorator(name='retrieve', decorator=swagger_auto_schema(
     tags=['Contact'],
-    operation_summary="Get interest object",
+    operation_summary="Get contact interest object",
     operation_description="""Show full information for interest."""
 ))
 @method_decorator(name='update', decorator=swagger_auto_schema(
     tags=['Contact'],
-    operation_summary="Update a interest list",
+    operation_summary="Update a contact interest object",
     operation_description="""Update interest object."""
 ))
 @method_decorator(name='partial_update', decorator=swagger_auto_schema(
@@ -125,16 +125,15 @@ class InterestViewSet(ModelViewSet):
     operation_summary="Partial Update a interest list",
     operation_description="""Partial Update interest object."""
 ))
-@method_decorator(name='create', decorator=swagger_auto_schema(
-    tags=['Contact'],
-    operation_summary="Create Interest object",
-    operation_description="""Create a new interest"""
-))
 @method_decorator(name='destroy', decorator=swagger_auto_schema(
     tags=['Contact'],
-    operation_summary="Delete Contact"
+    operation_summary="Delete a contact interest"
 ))
-class FilteredInterestViewSet(ModelViewSet):
+class FilteredInterestViewSet(mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   mixins.ListModelMixin,
+                   GenericViewSet):
     serializer_class = InterestSerializer
     queryset = Interest.objects.all()
     filterset_fields = {
